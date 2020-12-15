cceb510fafd88dcf76873deae15db84cc74e2dd7
oahul14 <oahul14@gmail.com>
2020-12-15 20:44:06 +0800

    new plots for both with platform and without platform

--- plotting.py
+++ plotting.py
@@ -20,44 +20,51 @@ def cal_mean_avg(df, pos, col_name):
     df.reindex(columns=sorted(df.columns))
     return df
 
-def plot_OA(r, fac):
-    pname = os.path.join(cur_dir, "Plots", "{}_{}-{}.png".format("OA", r[0], r[1]))
-    for i, n in enumerate(["NOP_Adf_0813.txt", "NOP_Adf_0815.txt", "NOP_Adf_0826.txt", "NOP_Adf_0828.txt"]):
-        dname = os.path.join(cur_dir, "FilteredData", "PlatformFiltered", n)
+def plot_OA(r, directory, fac, window):
+    pname = os.path.join(cur_dir, "Plots", "MA_{}_{}_{}-{}.png".format(directory, "OA", r[0], r[1]))
+    if window == 1:
+        pname = "N" + pname
+    for i, n in enumerate(["Adf_0813.txt", "Adf_0815.txt", "Adf_0826.txt", "Adf_0828.txt"]):
+        dname = os.path.join(cur_dir, "FilteredData", directory, n)
         fields = ["Latitude", "GPS_Alt", "ORG"]
         df = pd.read_csv(dname, usecols=fields, low_memory=False, skipinitialspace=True)
         df = filter_lat(df, r)
         df = cal_mean_avg(df, len(df)/fac, "ORG")
-        label = n.replace("NOP_Adf_", "OA ")
+        df['MA_alt'] = df['GPS_Alt'].rolling(window=window).mean()
+        df['MA_val'] = df["ORG"].rolling(window=window).mean()
+        label = n.replace("Adf_", "OA ")
         label = label.replace(".txt", "")
         plt.plot(df["ORG"], df['GPS_Alt'], c=cmap[i], label=label) # plot mean values
         plt.legend()
     plt.savefig(pname)
 
-def plot_others(r, tp, fac):
-    pname = os.path.join(cur_dir, "Plots", "MA_{}_{}-{}.png".format(tp, r[0], r[1]))
-    f_dir = os.path.join(cur_dir, "FilteredData", "PlatformFiltered")
+def plot_others(r, directory, tp, fac, window):
+    pname = os.path.join(cur_dir, "Plots", "MA_{}_{}_{}-{}.png".format(directory, tp, r[0], r[1]))
+    if window == 1:
+        pname = "N" + pname
+    f_dir = os.path.join(cur_dir, "FilteredData", directory)
     for i, n in enumerate(os.listdir(f_dir)):
         dname = os.path.join(f_dir, n)
         fields = ["Latitude", "GPS_Alt", params[tp]]
         df = pd.read_csv(dname, usecols=fields, low_memory=False, skipinitialspace=True)
         df = filter_lat(df, r)
         df = cal_mean_avg(df, len(df)/fac, params[tp])
-        df['MA_alt'] = df['GPS_Alt'].rolling(window=5).mean()
-        df['MA_val'] = df[params[tp]].rolling(window=5).mean()
-        label = n.replace("NOP_Adf_", tp+" ")
+        df['MA_alt'] = df['GPS_Alt'].rolling(window=window).mean()
+        df['MA_val'] = df[params[tp]].rolling(window=window).mean()
+        label = n.replace("Adf_", tp+" ")
         label = label.replace(".txt", "")
         plt.plot(df['MA_val'], df['MA_alt'], c=cmap[i], label=label) # plot mean values
         plt.legend()
     plt.savefig(pname)
+    plt.clf()
 
 if __name__ == "__main__":
     for i, r in enumerate(lat_range):
-        plot_others(r, "CCN", 100)
-        plt.clf()
-        plot_others(r, "CO", 100)
-        plt.clf()
-        plot_others(r, "BC", 100)
-        plt.clf()
-        plot_OA(r, 100)
-        plt.clf()
+        plot_others(r, "CombinedData", "CCN", 100, 5)
+        plot_others(r, "PlatformFiltered", "CCN", 100, 5)
+        plot_others(r, "CombinedData", "CO", 100, 5)
+        plot_others(r, "PlatformFiltered", "CO", 100, 5)
+        plot_others(r, "CombinedData", "BC", 100, 5)
+        plot_others(r, "PlatformFiltered", "BC", 100, 5)
+        plot_OA(r, "CombinedData", 100, 5)
+        plot_OA(r, "PlatformFiltered", 100, 5)
